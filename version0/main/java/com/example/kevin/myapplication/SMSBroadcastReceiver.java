651a1471fac23efa1a6c8f453b2509d71552497d
Colton Kopsa <coljamkop@gmail.com>
2015-06-27 14:33:46 -0600

    Parse SMS now working

--- version0/main/java/com/example/kevin/myapplication/SMSBroadcastReceiver.java
+++ version0/main/java/com/example/kevin/myapplication/SMSBroadcastReceiver.java
@@ -18,16 +18,39 @@ import static android.widget.Toast.makeText;
  */
 public class SMSBroadcastReceiver extends BroadcastReceiver {
 
+    private String mMessageBody; // stores message body
+    private String mGPScoords = "Hello World!"; // stores gps coordinates
+
     @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
             for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
-                String messageBody = smsMessage.getMessageBody();
-                Log.i("SMSmessage", messageBody);
+                mMessageBody = smsMessage.getMessageBody();
+                Log.i("SMSmessage", mMessageBody);
             }
             Toast message = makeText(context, "Orders recieved", LENGTH_SHORT);
             message.show();
+            parseSMSMessage();
+
+        }
+    }
+
+    //message parser for GPS coordinates
+    public void parseSMSMessage(){
+        boolean coordRecieved = false; // have we recieved the GPS coordinates?
+        mGPScoords = ""; // clear String
+        for (int i = 0; !coordRecieved; i++) {
+            // if you find a number
+            if (mMessageBody.charAt(i) >= '0' &&
+                    mMessageBody.charAt(i) <= '9') {
+                // add characters to the string until a space is found
+                for (int j = i; mMessageBody.charAt(j) != ' '; j++) {
+                    mGPScoords += mMessageBody.charAt(j);
+                }
+                coordRecieved = true; // we got what we came for
+                Log.e("GPSCoordinates", mGPScoords); // DEBUG: Check it!
+            }
         }
     }
 }
